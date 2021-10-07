af086b30ddccdf2228ab79500f485c69f32a0a5c
Ruofan1023 <e0563986@u.nus.edu>
2021-10-07 21:51:19 +0800

    Dummy commit

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -15,6 +15,7 @@ import seedu.address.model.student.UniquePersonList;
 public class AddressBook implements ReadOnlyAddressBook {
 
     private final UniquePersonList persons;
+    
 
     /*
      * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
